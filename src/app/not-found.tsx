18e1e1a6a215f6ad73752a0f9cd0b8d526077852
Fesyse <maksimlol.mail.com@mail.ru>
2024-09-29 06:27:54 +0300

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import Balancer from "react-wrap-balancer"
+import { Button } from "@/components/ui/button"
+import { getMainNav } from "@/lib/main-nav"
+
+export default function NotFoundPage() {
+  const items = getMainNav("")
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center px-4">
+      <h1 className="text-center text-xl sm:text-3xl">
+        <Balancer>404 - Страница не найдена</Balancer>
+      </h1>
+      <p className="text-center text-sm max-sm:max-w-96">
+        <Balancer>
+          Попробуйте перейти на другую страницу или повторить запрос
+        </Balancer>
+      </p>
+      <p className="mb-1 mt-4 md:mt-8">Перейти на</p>
+      <ul className="flex gap-4">
+        {items.map((item, i) => (
+          <li key={item.title}>
+            <Button
+              asChild
+              className="md:h-10 md:rounded-md md:px-8"
+              variant={i === 0 ? "default" : "secondary"}
+            >
+              <Link href={item.href}>{item.title}</Link>
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
