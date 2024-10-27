eab2deb33bc1b336cc67a0d89d6f2ab30611d7bd
shms0mms <shmsmms01@gmail.com>
2024-10-27 14:43:51 +0800

    feat: pass

--- src/components/pass/chappi-pass-map.tsx
+++ src/components/pass/chappi-pass-map.tsx
@@ -1,27 +1,81 @@
 "use client"
 
+import { DashboardIcon } from "@radix-ui/react-icons"
+import { BookA, Gift, Home } from "lucide-react"
 import { SyntheticEvent, WheelEvent, useEffect, useRef, useState } from "react"
+import { useMediaQuery } from "usehooks-ts"
 import { Reward } from "@/types/pass"
+import { MEDIA } from "@/config/media.config"
 import { Button } from "@/components/ui/button"
+import { ThemeToggle } from "../theme-toggle"
+import { Separator } from "../ui/separator"
 import {
   Sheet,
   SheetContent,
   SheetDescription,
+  SheetFooter,
   SheetHeader,
   SheetTitle
 } from "../ui/sheet"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "../ui/tooltip"
+import { ItemLink } from "./item-link"
 import NODES from "@/data/pass.json"
 
-export function ChappiPassMap({
-  handleWheel,
-  scale
-}: {
-  handleWheel: (e: WheelEvent<HTMLCanvasElement>) => void
-  scale: number
-}) {
+const TableItem = ({ description, id, title, completed }: Reward) => {
+  return (
+    <>
+      {" "}
+      <div key={id} className="w-full">
+        <div className="flex w-full items-center justify-between gap-2">
+          <div className="flex flex-col">
+            <figcaption className="text-sm font-medium dark:text-white">
+              {title}
+            </figcaption>
+            <figcaption className="font-norml text-xs dark:text-white">
+              {description}
+            </figcaption>
+          </div>{" "}
+          <TooltipProvider>
+            {" "}
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  disabled={completed}
+                  className="aspect-square h-auto w-8"
+                  size={"icon"}
+                  type="button"
+                >
+                  <Gift size={14} />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Собрать награду</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
+      <Separator />
+    </>
+  )
+}
+export function ChappiPassMap() {
+  const [scale, setScale] = useState(1)
+  const handleZoomIn = () => setScale(prevScale => Math.min(prevScale * 1.1, 3))
+  const handleZoomOut = () =>
+    setScale(prevScale => Math.max(prevScale * 0.9, 0.5))
+  const handleWheel = (e: WheelEvent<HTMLCanvasElement>) => {
+    const zoom = e.deltaY > 0 ? 0.9 : 1.1
+    setScale(prevScale => Math.max(0.5, Math.min(prevScale * zoom, 3))) // Ограничиваем масштаб
+  }
   // Утилиты для начала создания canvas
   const COMPLETED_COLOR = "#478049"
-  const SHEET_SIZE = 500
+  const isDesktop = useMediaQuery(MEDIA.lg)
+  const SHEET_SIZE = isDesktop ? 0 : 500
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null)
   const [nodes, setNodes] = useState<Reward[]>(NODES as Reward[])
@@ -52,7 +106,7 @@ export function ChappiPassMap({
     return () => {
       window.removeEventListener("resize", resizeCanvas)
     }
-  }, [])
+  }, [isDesktop])
 
   // Рисование карты
   useEffect(() => {
@@ -86,7 +140,17 @@ export function ChappiPassMap({
     })
 
     nodes.forEach(node => {
-      drawNode(ctx, node.x, node.y, node.title, node.completed, node.type)
+      drawNode(ctx, {
+        completed: node.completed,
+        size: node.size,
+        y: node.y,
+        x: node.x,
+        type: node.type,
+        title: node.title,
+        id: node.id,
+        connectedTo: node.connectedTo,
+        description: node.description
+      })
     })
 
     ctx.restore()
@@ -118,15 +182,11 @@ export function ChappiPassMap({
   // Рисование самого point
   const drawNode = (
     ctx: CanvasRenderingContext2D,
-    x: number,
-    y: number,
-    label: string,
-    completed: boolean,
-    type: string
+    { completed, size, y, x, type, title }: Reward
   ) => {
     ctx.beginPath()
     let radius = 60
-    if (type === "bigReward") {
+    if (size === "big") {
       radius = 90
       ctx.arc(x + SHEET_SIZE, y, radius - 5, 0, Math.PI * 2)
       ctx.fillStyle = completed ? COMPLETED_COLOR : "#333"
@@ -140,9 +200,9 @@ export function ChappiPassMap({
     ctx.fillStyle = "#fff"
     ctx.textAlign = "center"
     ctx.textBaseline = "middle"
-    ctx.font = `${type === "bigReward" ? "bold 32px" : "24px"}`
+    ctx.font = `${size === "big" ? "22px" : "16px"} Arial`
 
-    const words = label.split(" ")
+    const words = title.split(" ")
     let line = ""
     let lines = []
     for (let i = 0; i < words.length; i++) {
@@ -240,6 +300,14 @@ export function ChappiPassMap({
         onMouseUp={handleMouseUp}
         onWheel={handleWheel}
       />
+      <div className="fixed right-6 top-1/2 flex w-[50px] -translate-y-1/2 flex-col justify-center p-2 max-lg:hidden">
+        <button onClick={handleZoomIn} className="mb-1 rounded-md bg-muted p-2">
+          +
+        </button>
+        <button onClick={handleZoomOut} className="rounded-md bg-muted p-2">
+          -
+        </button>
+      </div>
       <Button
         size={"lg"}
         className="fixed bottom-4 left-1/2 -translate-x-1/2"
@@ -247,12 +315,14 @@ export function ChappiPassMap({
       >
         Собрать все доступные награды
       </Button>
-      <Sheet open={true} modal={false}>
+      <Sheet open={isDesktop ? !!selectedNode : true} modal={false}>
         <SheetContent
-          isCloseButton={false}
+          onClose={() => setSelectedNode(undefined)}
+          isCloseButton={isDesktop ? true : false}
           side={"left"}
-          className="absolute left-0 top-0"
+          className="absolute left-0 top-0 flex flex-col gap-5 overflow-auto p-3"
         >
+          {" "}
           <div className="flex flex-col gap-5">
             <SheetHeader>
               <SheetTitle>{selectedNode?.title || nodes[0]?.title}</SheetTitle>
@@ -268,6 +338,47 @@ export function ChappiPassMap({
               {selectedNode?.completed ? "Получено" : "Получить"}
             </Button>
           </div>
+          <div className="flex flex-col gap-5 md:rounded-md md:bg-muted/25 md:p-4">
+            <Tabs defaultValue={"completed"}>
+              <TabsList className="w-full">
+                <TabsTrigger className="w-full" value="completed">
+                  Собранные
+                </TabsTrigger>
+                <TabsTrigger className="w-full" value="incompleted">
+                  Не собранные
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent className="flex flex-col gap-4" value="completed">
+                {nodes
+                  .filter(n => n.completed)
+                  .map(node => (
+                    <TableItem {...node} key={node.id} />
+                  ))}
+              </TabsContent>
+              <TabsContent className="flex flex-col gap-4" value="incompleted">
+                {nodes
+                  .filter(n => !n.completed)
+                  .map(node => (
+                    <TableItem {...node} key={node.id} />
+                  ))}
+              </TabsContent>
+            </Tabs>
+          </div>
+          <div className="h-full" />
+          <SheetFooter className="flex w-full flex-row items-center justify-between gap-5 sm:flex-row sm:justify-between">
+            <ul className="flex items-center gap-4">
+              <ItemLink content="Главная" href={"/"}>
+                <Home />
+              </ItemLink>
+              <ItemLink content="Личный кабинет" href={"/dashboard"}>
+                <DashboardIcon />
+              </ItemLink>
+              <ItemLink content="Книги" href={"/books"}>
+                <BookA />
+              </ItemLink>
+            </ul>
+            <ThemeToggle />{" "}
+          </SheetFooter>
         </SheetContent>
       </Sheet>
     </>
