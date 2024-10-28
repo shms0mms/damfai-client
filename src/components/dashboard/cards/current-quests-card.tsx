cb06369edfd8ed4395aa8e99b1c251e70f007ef7
shms0mms <shmsmms01@gmail.com>
2024-10-29 01:54:58 +0800

    feat: current quests & finally version of dashboard

--- src/components/dashboard/cards/current-quests-card.tsx
+++ src/components/dashboard/cards/current-quests-card.tsx
@@ -1,4 +1,4 @@
-import { Gift } from "lucide-react"
+import { Check, Gift } from "lucide-react"
 import { ICON_MAP } from "@/types/pass"
 import { Button } from "@/components/ui/button"
 import { Progress } from "@/components/ui/progress"
@@ -13,42 +13,46 @@ export async function CurrentQuestsCard() {
       className="col-span-1"
       subtitle={"Просмотрите текущих квестов на сегодня"}
     >
-      <ul className="flex max-h-[380px] flex-col gap-5 overflow-auto">
-        {data?.length
-          ? [...data, ...data].map(d => {
-              const Icon = ICON_MAP[d.icon]
-              return (
-                <li
-                  key={d.id}
-                  className="flex items-center gap-5 border-b border-solid pb-3"
-                >
-                  <div className="flex w-full items-center gap-5">
-                    <Icon className="min-h-[16px] min-w-[16px]" />
-                    <div className="flex flex-col gap-1.5">
-                      <div className="flex items-center gap-3">
-                        <h2 className="truncate">{d.title}</h2>
-                        <Progress
-                          value={(d.progress / d.total) * 100}
-                          className="h-1 min-w-[80px] max-w-[80px] max-md:hidden"
-                        />
-                        <p className="whitespace-nowrap text-sm text-muted-foreground">
-                          {d.progress} / {d.total}
-                        </p>
-                      </div>
-                      <p>{d.description}</p>
+      <ul className="flex max-h-[380px] w-full flex-col gap-5 overflow-auto">
+        {data?.length ? (
+          [...data, ...data].map(d => {
+            const Icon = ICON_MAP[d.icon]
+            return (
+              <li
+                key={d.id}
+                className="flex items-center gap-5 border-b border-solid pb-3"
+              >
+                <div className="flex w-full items-center gap-5">
+                  <Icon className="min-h-[16px] min-w-[16px]" />
+                  <div className="flex flex-col gap-1.5">
+                    <div className="flex items-center gap-3">
+                      <h2 className="truncate">{d.title}</h2>
+                      <Progress
+                        value={(d.progress / d.total) * 100}
+                        className="h-1 min-w-[80px] max-w-[80px] max-md:hidden"
+                      />
+                      <p className="whitespace-nowrap text-sm text-muted-foreground">
+                        {d.progress} / {d.total}
+                      </p>
                     </div>
+                    <p>{d.description}</p>
                   </div>
-                  <Button
-                    disabled={d.progress >= d.total}
-                    size={"icon"}
-                    variant="outline"
-                  >
+                </div>
+                <Button disabled={true} size={"icon"} variant="outline">
+                  {d.progress >= d.total ? (
+                    <Check size={14} />
+                  ) : (
                     <Gift size={14} />
-                  </Button>
-                </li>
-              )
-            })
-          : null}
+                  )}
+                </Button>
+              </li>
+            )
+          })
+        ) : (
+          <div className="flex min-h-[380px] w-full items-center justify-center">
+            Пока ничего нет
+          </div>
+        )}
       </ul>
     </CardWrapper>
   )
