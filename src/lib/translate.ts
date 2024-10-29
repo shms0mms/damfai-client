6bb213fb6627aac1d23490148229e5d6a660ad3a
fesyse <maksimlol.mail.com@mail.ru>
2024-10-29 15:31:01 +0800

    feat: translate books per year pie chart

---
+++ src/lib/translate.ts
@@ -0,0 +1,14 @@
+export const translateMonths = {
+  January: "Январь",
+  February: "Февраль",
+  March: "Март",
+  April: "Апрель",
+  May: "Май",
+  June: "Июнь",
+  July: "Июль",
+  August: "Август",
+  September: "Сентябрь",
+  October: "Октябрь",
+  November: "Ноябрь",
+  December: "Декабрь"
+} as const
