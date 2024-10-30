3bb52128ef53c8ded173bd0a8381eebb789b852f
shms0mms <shmsmms01@gmail.com>
2024-10-30 17:33:14 +0800

    test: jest -> vitest -> comfort testing

--- src/__test__/button.test.tsx
+++ src/__test__/button.test.tsx
@@ -1,11 +1,12 @@
-import "@testing-library/jest-dom"
+import "@testing-library/dom"
 import { fireEvent, render, screen } from "@testing-library/react"
 import React from "react"
+import { describe, expect, it, vi } from "vitest"
 import { Button } from "../components/ui/button"
 
 describe("Button component", () => {
   it("calls onClick handler when clicked", () => {
-    const handleClick = jest.fn()
+    const handleClick = vi.fn()
     render(<Button onClick={handleClick}>Click me</Button>)
 
     fireEvent.click(screen.getByRole("button")) // Убедитесь, что у вашей кнопки есть роль "button"
