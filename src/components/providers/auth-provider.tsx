d5b4c591caba1a0f17bada83087cc276529d1dbe
shms0mms <shmsmms01@gmail.com>
2024-11-01 23:34:30 +0800

    fix: logout

--- src/components/providers/auth-provider.tsx
+++ src/components/providers/auth-provider.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { useQueryClient } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { createContext, useEffect } from "react"
 import { User } from "@/types/user"
-import { useProfile } from "@/hooks/useProfile"
 import { getAccessToken, removeAccessTokenFromStorage } from "@/lib/auth"
+import { authService } from "@/services/auth.service"
 
 type AuthContext = {
   user?: User
@@ -17,17 +17,25 @@ export const AuthContext = createContext<AuthContext>({} as AuthContext)
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const accessToken = getAccessToken()
-  const { data: user, refetch, isLoading } = useProfile()
+  const {
+    data: user,
+    refetch,
+    isLoading
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => authService.me(),
+    retry: false
+  })
   const router = useRouter()
   const queryClient = useQueryClient()
-  const logout = () => {
+  const logout = async () => {
     localStorage.removeItem("readTime")
     localStorage.removeItem("lastReadBook")
     removeAccessTokenFromStorage()
-
-    refetch()
+    queryClient.setQueryData(["user"], null)
     router.push("/")
   }
+
   useEffect(() => {
     accessToken && refetch()
   }, [accessToken])
